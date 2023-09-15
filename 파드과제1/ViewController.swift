c47380cb8dd3c24427e853834a211a29c95e78a6
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-09-15 18:29:20 +0900

    first commit

---
+++ 파드과제1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  파드과제1
+//
+//  Created by hyungjin kim on 2023/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
