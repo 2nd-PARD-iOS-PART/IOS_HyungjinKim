04ba19a6fbe43377cc336acd2b17c372263b55fc
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-10-07 10:01:51 +0900

    redo 3rd HW

---
+++ 3rd_homework_KimHyungjin/3주차 redo/3rd_redo/3rd_redo/CollectionInTabelViewCell.swift
@@ -0,0 +1,77 @@
+//  CollectionInTableViewCell.swift
+//  3rd_homework_KimHyungjin
+//
+//  Created by hyungjin kim on 2023/10/07.
+//
+
+import UIKit
+
+class CollectionInTableViewCell: UITableViewCell, UICollectionViewDelegateFlowLayout {
+    
+    var models: [Model] = []
+    var tableViewSection: Int = 0
+
+   static let identifier = "CollectionInTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    func configure(with models: [Model]) {
+        self.models = models
+        collectionView.reloadData()
+    }
+}
+
+extension CollectionInTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as? MyCollectionViewCell else {
+            fatalError("Could not dequeue MyCollectionViewCell")
+        }
+        
+        let model = models[indexPath.item]
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        switch tableViewSection {
+        case 0:
+            return CGSize(width: 150, height: 170)
+        case 1:
+            return CGSize(width: 126, height: 227)
+        default:
+            return CGSize(width: 125, height: 200) // Default size
+        }
+        
+    }
+}
---
+++ 3rd_homework_KimHyungjin/3주차 redo/3rd_redo/3rd_redo/HeroHeaderUI.swift
@@ -0,0 +1,63 @@
+//
+//  HeroHeader.swift
+//  3rd_homework_KimHyungjin
+//
+//  Created by hyungjin kim on 2023/10/07.
+//
+
+import UIKit
+
+class HeroHeaderUI: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(titleImage)
+        applyConstraints()
+    }
+    
+    private func applyConstraints(){
+        NSLayoutConstraint.activate([
+            titleImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleImage.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 100),
+            titleImage.widthAnchor.constraint(equalToConstant: 300),
+            titleImage.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "HeroImage")
+        return imageView
+    }()
+    
+    private let titleImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Stranger Things")
+        imageView.contentMode = .scaleAspectFit 
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
